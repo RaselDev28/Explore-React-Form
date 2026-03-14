f99e22c797f71de7a31506859d26e7d2f80f3a7d
RaselDev28 <rasel.dev28@gmail.com>
2026-03-15 00:13:11 +0600

    form

--- src/Components/productManagement/ProductForm.jsx
+++ src/Components/productManagement/ProductForm.jsx
@@ -1,35 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const ProductForm = () => {
+const ProductForm = ({ handleAddProducts }) => {
 
-    const handleProductSubmit = e =>{
+    const [error, setError] = useState('')
+
+    const handleProductSubmit = e => {
         e.preventDefault();
-        const name=e.target.name.value;
-        const price=e.target.price.value;
-        const quantity=e.target.quantity.value;
+        const name = e.target.name.value;
+        const price = e.target.price.value;
+        const quantity = e.target.quantity.value;
 
         // console.log(name, price, quantity)
+        // Validation
+        if (name.length === 0) {
+            setError("Please provide a product name")
+            return;
+        }
+        else if (price.length === 0) {
+            setError("Please Provide a Price");
+            return;
+        }
+        else if (price < 0) {
+            setError('Please Provide a positive number')
+            return;
+        }
+        else if (quantity < 0) {
+            setError("Please add a quantity");
+            return;
+        }
+        else if (quantity.length === 0) {
+            setError('please give a number');
+            return;
+        }
+        else {
+            setError('');
+        }
 
-        const newProduct={
+        const newProduct = {
             name,
             price,
             quantity
         }
-        console.log(newProduct)
+        // console.log(newProduct)
+
+        handleAddProducts(newProduct);
+
     }
 
     return (
         <div>
             <h2>Add a Product</h2>
             <form onSubmit={handleProductSubmit}>
-                <input type="text" name="name" placeholder='Product name'/>
+                <input type="text" name="name" placeholder='Product name' />
                 <br />
-                <input type="text" name="price" placeholder='Product Price'/>
+                <input type="text" name="price" placeholder='Product Price' />
                 <br />
-                <input type="text" name="quantity" placeholder='Product Quantity'/>
+                <input type="text" name="quantity" placeholder='Product Quantity' />
                 <br />
                 <input type="submit" value="Submit" />
             </form>
+            <p style={{ color: 'red' }}><small>{error}</small></p>
         </div>
     );
 };
